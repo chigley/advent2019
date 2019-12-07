502f48c9e06f012e89d6bbe344dc4bfdbb6f1efa
Charlie Hothersall-Thomas <me@charlie.ht>
2019-12-07 10:50:10 +0000

    Add day 7 part two

--- cmd/day7/main.go
+++ cmd/day7/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"sync"
 
 	"github.com/chigley/advent2019"
 	"github.com/chigley/advent2019/intcode"
@@ -13,7 +14,10 @@ import (
 
 const amplifiers = 5
 
-var phaseSettings = []int{0, 1, 2, 3, 4}
+var (
+	part1PhaseSettings = []int{0, 1, 2, 3, 4}
+	part2PhaseSettings = []int{5, 6, 7, 8, 9}
+)
 
 func main() {
 	program, err := advent2019.ReadIntsLine(os.Stdin)
@@ -26,15 +30,35 @@ func main() {
 		log.Fatal(err)
 	}
 
+	part2, err := Part2(program)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(part1)
+	fmt.Println(part2)
 }
 
 func Part1(program []int) (int, error) {
 	comp := intcode.New(program)
 	maxOutput := math.MinInt64
 
-	for settings := range advent2019.Permutations(phaseSettings) {
+	for settings := range advent2019.Permutations(part1PhaseSettings) {
 		output, err := outputSignal(comp, settings)
+		if err != nil {
+			return 0, err
+		}
+		maxOutput = advent2019.Max(maxOutput, output)
+	}
+
+	return maxOutput, nil
+}
+
+func Part2(program []int) (int, error) {
+	maxOutput := math.MinInt64
+
+	for settings := range advent2019.Permutations(part2PhaseSettings) {
+		output, err := feedbackLoopOutputSignal(program, settings)
 		if err != nil {
 			return 0, err
 		}
@@ -58,4 +82,48 @@ func outputSignal(comp *intcode.Computer, settings []int) (int, error) {
 	}
 
 	return input, nil
+}
+
+func feedbackLoopOutputSignal(program, settings []int) (int, error) {
+	amps := make([]intcode.Computer, amplifiers)
+	initialInput := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(amplifiers)
+	done := func() {
+		wg.Done()
+	}
+
+	input := initialInput
+	for i := 0; i < amplifiers; i++ {
+		amps[i] = *intcode.New(program)
+
+		tmp := input
+		input = amps[i].RunInteractive(input, done)
+		tmp <- settings[i]
+	}
+
+	// Relay the last amplifier's output into the first.
+	go func() {
+		for out := range input {
+			initialInput <- out
+		}
+	}()
+
+	// Pass 0 to the first amplifier.
+	initialInput <- 0
+
+	wg.Wait()
+	for _, amp := range amps {
+		if err := amp.Err(); err != nil {
+			return 0, err
+		}
+	}
+
+	// Our result comes from our first amplifier's input channel, which is
+	// equivalent to our last amplifier's output channel.
+	out := <-initialInput
+	close(initialInput)
+
+	return out, nil
 }
--- cmd/day7/main_test.go
+++ cmd/day7/main_test.go
@@ -18,6 +18,23 @@ var part1Tests = []struct {
 	{[]int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0}, 65210},
 }
 
+var part2Tests = []struct {
+	program []int
+	output  int
+}{
+	{
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4,
+			27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		139629729,
+	},
+	{
+		[]int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55,
+			1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008,
+			54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56,
+			1005, 56, 6, 99, 0, 0, 0, 0, 10}, 18216,
+	},
+}
+
 func TestDay7(t *testing.T) {
 	input, err := os.Open("testdata/input")
 	if err != nil {
@@ -35,12 +52,29 @@ func TestDay7(t *testing.T) {
 		t.Error(err)
 	}
 
+	part2, err := day7.Part2(program)
+	if err != nil {
+		t.Error(err)
+	}
+
 	assert.Equal(t, 880726, part1)
+	assert.Equal(t, 4931744, part2)
+
 }
 
 func TestPart1(t *testing.T) {
 	for _, tt := range part1Tests {
 		output, err := day7.Part1(tt.program)
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, tt.output, output)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	for _, tt := range part2Tests {
+		output, err := day7.Part2(tt.program)
 		if err != nil {
 			t.Error(err)
 		}
